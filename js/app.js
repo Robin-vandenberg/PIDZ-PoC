b4ba164b1582690bfcc59d5d3f4a4bd8134472f4
Robin van den Berg <rob-vandenberg@outlook.com>
2020-04-08 13:44:04 +0200

    Added datepicker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 // On window Load
 $(()=>{
     console.log( "Document loaded" );
-    
+
+    // Init datepicker
+    $( "#form_searches_0_daterange" ).datepicker();
+
+    //Init detail modal
     $("#detailsToggler").click(function(){
         $(this).toggleClass("collapsed");
         $("#detail-row-0").toggleClass("collapse")
