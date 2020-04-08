b268453ddc576dfc41a5ef09f50be065b84b0f9b
Robin van den Berg <rob-vandenberg@outlook.com>
2020-04-08 13:34:32 +0200

    Formatted html + fixed drop down

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // On window Load
 $(()=>{
     console.log( "Document loaded" );
-    $( "#datePicker" ).datepicker();
+    
+    $("#detailsToggler").click(function(){
+        $(this).toggleClass("collapsed");
+        $("#detail-row-0").toggleClass("collapse")
+    });
 })
