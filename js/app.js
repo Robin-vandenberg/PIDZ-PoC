9a8d6bea697e30541aab5e32c02a4663267dd813
Robin van den Berg <rob-vandenberg@outlook.com>
2020-04-14 13:01:34 +0200

    added nudge dialog

--- js/app.js
+++ js/app.js
@@ -8,14 +8,41 @@ $(()=>{
 		$( "#form_searches_0_functie" ).selectmenu();
 	} );
 	
-	$( "#form_searches_0_functie" ).on( "selectmenuchange", function( event, ui ) {callNudge();} );
+	$( "#form_searches_0_functie" ).on( "selectmenuchange", ( event, ui ) => { callNudge(); });
 	
     // Init datepicker
-    $( "#form_searches_0_daterange" ).datepicker();
+    $( "#form_searches_0_daterange").datepicker();
+    $( "#form_searches_1_daterange").datepicker();
 
     //Init detail modal
     $("#detailsToggler").click(function(){
         $(this).toggleClass("collapsed");
         $("#detail-row-0").toggleClass("collapse")
     });
+
+    //Init Nudge modal
+    $( "#dialog-message" ).dialog({
+        autoOpen: true,
+        modal: false,
+        position: { my: "left top", at: "left bottom", of: '#nudgeDiv' },
+        resizable: false,
+        draggable: false,
+        closeOnEscape: true,
+        buttons: {
+          Nee: function() {
+            $( this ).dialog( "close" );
+          },
+          Ja: function() {
+            $( this ).dialog( "close" );
+            setPublicationDate();
+          }
+        }
+      });
+
+    //! Test loading data | This is a development function and needs to be removed in production
+    CreateNudge('verpleegkundige');
 })
+
+function setPublicationDate() {
+
+}
